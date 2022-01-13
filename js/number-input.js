30a46f520ca7f45fd66f82e261f4b913bee0de1b
losbiw <skorinov41@gmail.com>
2022-01-13 06:22:23 +0200

    don't allow less than 1 entry

--- js/number-input.js
+++ js/number-input.js
@@ -1,13 +1,16 @@
+const colorElements = document.querySelectorAll('.color');
+const price = document.querySelector('.price');
+
 const colorsCount = {
-    mint: 0,
+    mint: 1,
     purple: 0,
     blue: 0,
 }
 
-const colorElements = document.querySelectorAll('.color');
+const calculateEntriesAmount = () => Object.keys(colorsCount).reduce((acc, curr) => acc + colorsCount[curr], 0);
+const calculatePrice = () => `$${calculateEntriesAmount() * 85}.00`;
 
 colorElements.forEach(color => {
-
     const decreaseButton = color.querySelector('button:first-of-type');
     const increaseButton = color.querySelector('button:last-of-type');
     const countText = color.querySelector('p');
@@ -15,13 +18,22 @@ colorElements.forEach(color => {
     decreaseButton.onclick = () => {
         if (colorsCount[color.classList[1]] - 1 >= 0) {
             colorsCount[color.classList[1]] -= 1;
+
+            if (calculateEntriesAmount() < 1) {
+                colorsCount[color.classList[1]] += 1;
+            }
+
             countText.textContent = colorsCount[color.classList[1]];
+
+
+            price.textContent = calculatePrice();
         }
     }
 
     increaseButton.onclick = () => {
         colorsCount[color.classList[1]] += 1;
         countText.textContent = colorsCount[color.classList[1]];
-        console.log(colorsCount);
+        
+        price.textContent = calculatePrice();
     }
 });
